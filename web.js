5ac69737b4f16d365cea557f8b44b97208b8cb37
JayLine <jaylinedev@gmail.com>
2013-07-10 23:10:30 +0000

    Reading the index.html file and showing it

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+buffer = new Buffer(256);
+buffer.write(fs.readFileSync('index.html',0));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
